43b06af06f145043cb4b275ee791bc8adca85ded
Swizec Teller <swizec@swizec.com>
2023-11-28 07:26:46 -0800

    Add explanation comments

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,10 +1,24 @@
 exports.createPages = async ({ actions }) => {
+    /**
+     * Gatsby proxy: https://www.gatsbyjs.com/docs/how-to/cloud/working-with-redirects-and-rewrites/#rewrites-and-reverse-proxies
+     *
+     * We expect this redirect to return a 200,
+     * but serve content from the toPath.
+     *
+     * Vercel returns a 307 and redirects you to the toPath
+     */
     actions.createRedirect({
         fromPath: "/stats/js/script.js",
         toPath: "https://plausible.io/js/plausible.js",
         statusCode: 200,
     });
 
+    /**
+     * Gatsby redirect: https://www.gatsbyjs.com/docs/how-to/cloud/working-with-redirects-and-rewrites/#directions
+     *
+     * We expect this to return a 301 and redirect you to the toPath
+     * Vercel returns a 404 and serves pages/404.js
+     */
     actions.createRedirect({
         fromPath: "/i-dont-exist",
         toPath: "/hello",
